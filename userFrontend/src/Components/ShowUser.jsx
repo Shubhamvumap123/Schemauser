c9fafd18f5a39d944cf0dff213774fd8dcffca69
Shubhamvumap123 <94427228+Shubhamvumap123@users.noreply.github.com>
2022-06-09 15:23:35 +0530

    comit

--- userFrontend/src/Components/ShowUser.jsx
+++ userFrontend/src/Components/ShowUser.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import { useSearchParams } from 'react-router-dom'
 const ShowUser = () => {
+let [searchParams, setSearchParams] = useSearchParams();
+
+
   return (
     <div>
-        
+      <form onSubmit={handleSubmit}>{/* ... */}</form>
     </div>
-  )
+  );
 }
 
 export default ShowUser
